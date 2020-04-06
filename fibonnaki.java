de62a4e9a74878f105195f7791f23c5b59751dfc
mehediw3 <mehediw3@gmail.com>
2020-04-06 22:22:48 +0600

    Create fibonnaki.java

---
+++ fibonnaki.java
@@ -0,0 +1,23 @@
+
+package arraytwo;
+
+public class ArrayTwo {
+
+    public static void main(String[] args) {
+        int c;
+        int b;
+        int i = 0;
+        int j = 1;
+        int a ;
+        for(int n = 0;n<= 100;n++){
+            a = i + j;
+            i = j;
+            j = a;
+            n = i+j;
+            
+            System.out.println(i+" + "+j+" = "+n);
+           
+        }
+    }
+    
+}
